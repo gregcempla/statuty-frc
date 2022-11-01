aeaeec7ee522060a9cab23bb04d4b00e8bcbcbce
Grzegorz Cempla <grzegorz.cempla@c02d37w1md6m.lan>
2022-11-01 20:55:32 +0100

    Generowanie wyłączone bo cały tekst przeniesiony do git

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-curl -L 'https://docs.google.com/document/d/1ByGD3GyJd0wZBju3RFxavCDAlq0nMpDWGERurxmyzk0/export?format=txt' -o statuty-frc.tex ; pdflatex statuty-frc.tex; pdflatex statuty-frc.tex; pdflatex statuty-frc.tex
-
-curl -L 'https://docs.google.com/document/d/1g9ErsYqFnznivG0ytcTNWpthWX0frIDFwzPGqq-qFaA/export?format=txt' -o przepisy-wiernych-stowarzyszonych.tex; pdflatex przepisy-wiernych-stowarzyszonych.tex; pdflatex przepisy-wiernych-stowarzyszonych.tex; pdflatex przepisy-wiernych-stowarzyszonych.tex
+#curl -L 'https://docs.google.com/document/d/1ByGD3GyJd0wZBju3RFxavCDAlq0nMpDWGERurxmyzk0/export?format=txt' -o statuty-frc.tex
+#curl -L 'https://docs.google.com/document/d/1g9ErsYqFnznivG0ytcTNWpthWX0frIDFwzPGqq-qFaA/export?format=txt' -o przepisy-wiernych-stowarzyszonych.tex
+#curl -L 'https://docs.google.com/document/d/1LbriDEwGMnYV8PtxiKYSur4Gwa9jNEAxpt7TflqKRPI/export?format=txt' -o przepisy-ogolne.tex
